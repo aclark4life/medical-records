60d62ece149d206a99d83e9d764de635dff548a2
Jeffrey A. Clark <aclark@aclark.net>
2026-02-08 20:35:12 -0500

    Fix app

--- medical_records/apps.py
+++ medical_records/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MedicalRecordsConfig(AppConfig):
     default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
-    name = "demo.medical_records"
+    name = "medical_records"
