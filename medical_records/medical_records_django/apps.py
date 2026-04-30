0d88ba650ee49771b26d95ffd373bbe1e4cacf4a
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-04-29 22:19:31 -0400

    Set app label to medical_records_django

--- medical_records/medical_records_django/apps.py
+++ medical_records/medical_records_django/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MedicalRecordsDjangoConfig(AppConfig):
     default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
     name = "medical_records.medical_records_django"
-    label = "medical_records"
+    label = "medical_records_django"
